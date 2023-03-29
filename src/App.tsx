60ea59797ba75605fa97f652ba4883a23ff4bda4
Nik <kaden0065@gmail.com>
2023-03-29 16:14:02 +0300

    add prompt about mobile app

--- src/App.tsx
+++ src/App.tsx
@@ -7,39 +7,59 @@ import Footer from './components/Footer/Footer';
 import ShoppingCartPage from './pages/ShoppingCart';
 import CheckoutPage from './pages/Checkout';
 import NotFound from './components/NotFound/NotFound';
+import ShowMobile from './components/ShowMobile/ShowMobile';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
+
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<MainPage />}>
-            <Route path='/New-Arrivals' />
-            <Route path='/Sale' />
-            <Route path='/House-Plants' />
-            <Route path='/Potter-Plants' />
-            <Route path='/Seeds' />
-            <Route path='/Small-Plants' />
-            <Route path='/Big-Plants' />
-            <Route path='/Succulents' />
-            <Route path='/Terrariums' />
-            <Route path='/Gardening' />
-            <Route path='/Accessions' />
-            <Route path='/Small' />
-            <Route path='/Medium' />
-            <Route path='/Large' />
-            <Route path='/Price/:price' />
-            <Route path='/Login' />
-            <Route path='/Register' />
-          </Route>
-          <Route path='/Product/:productId' element={<ProductPage />} />
-          <Route path='/Shopping-Cart' element={<ShoppingCartPage />} />
-          <Route path='/Checkout' element={<CheckoutPage />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {
+        window.innerWidth <= 1000 ?
+          <ShowMobile /> :
+          <BrowserRouter>
+            <Header />
+            <Routes>
+              <Route path='/' element={<MainPage />}>
+                <Route path='/New-Arrivals' />
+                <Route path='/Sale' />
+                <Route path='/House-Plants' />
+                <Route path='/Potter-Plants' />
+                <Route path='/Seeds' />
+                <Route path='/Small-Plants' />
+                <Route path='/Big-Plants' />
+                <Route path='/Succulents' />
+                <Route path='/Terrariums' />
+                <Route path='/Gardening' />
+                <Route path='/Accessions' />
+                <Route path='/Small' />
+                <Route path='/Medium' />
+                <Route path='/Large' />
+                <Route path='/Price/:price' />
+                <Route path='/Login' />
+                <Route path='/Register' />
+              </Route>
+              <Route path='/Product/:productId' element={<ProductPage />} />
+              <Route path='/Shopping-Cart' element={<ShoppingCartPage />} />
+              <Route path='/Checkout' element={<CheckoutPage />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+      }
     </div>
   );
 };
---
+++ src/components/ShowMobile/ShowMobile.tsx
@@ -0,0 +1,9 @@
+import {FC} from 'react';
+
+const ShowMobile: FC = () => {
+    return (
+        <h1 className="mobile">We have mobile app</h1>
+    );
+};
+
+export default ShowMobile;
